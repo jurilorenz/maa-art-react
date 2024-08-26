616d4aae2442d24a2833c912a3584b137164e86f
jurilorenz <114692921+jurilorenz@users.noreply.github.com>
2024-08-26 19:05:33 +0200

    Initial commit - Set up React project with Tailwind. Adjust text hero page.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Hero from './components/Hero.jsx';
+import Navbar from './components/Hero.jsx';
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
