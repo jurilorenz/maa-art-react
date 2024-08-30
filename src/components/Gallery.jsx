d65e16f12c8d174d642be5a1649b94fae5f65186
jurilorenz <114692921+jurilorenz@users.noreply.github.com>
2024-08-30 15:14:52 +0200

    Add modal component and adjust pic width and layout of arrows.

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { BsChevronLeft, BsChevronRight } from 'react-icons/bs';
-import { VscChromeClose } from "react-icons/vsc";
+import Modal from './Modal'; // Import the Modal component
 
 // Dynamically load all images from the thumbnails directory
 const importAll = (r) => {
@@ -126,53 +125,16 @@ const Gallery = () => {
         ))}
       </div>
 
-      {/* Modal */}
-{selectedIndex !== null && (
-  <div 
-    className='fixed inset-0 bg-black/90 z-50 flex items-center justify-center'
-    onClick={closeModal} // Close the modal when clicking on the background
-  >
-    <button 
-      onClick={closeModal} 
-      className='absolute top-2 right-2 text-white text-3xl hover:text-gray-300 bg-transparent border-none'
-    >
-      <VscChromeClose />
-    </button>
-
-    <div className='relative max-w-[80%] max-h-[80%] flex flex-col justify-center items-center'>
-      <img 
-        src={filteredImages[selectedIndex].fullSrc} 
-        alt={filteredImages[selectedIndex].title} 
-        className='object-contain max-w-full max-h-[80vh]' 
-      />
-
-      <div className='mt-4 text-white text-center flex items-center justify-center lg:justify-between w-full px-4'>
-        {/* Left Arrow (mobile version next to text, large screen on sides) */}
-        <button 
-          onClick={showPrevImage} 
-          className='text-white text-3xl hover:text-gray-300 bg-transparent border-none mr-4 lg:mr-0 lg:absolute lg:left-4 lg:top-1/2 lg:transform lg:-translate-y-1/2 lg:-translate-x-[calc(100%+1rem)]' // Stick to the left border
-        >
-          <BsChevronLeft />
-        </button>
-
-        {/* Title and Info */}
-        <div className='flex flex-col items-center lg:flex-grow'>
-          <p className='text-lg font-bold'>{filteredImages[selectedIndex].title}</p>
-          <p className='text-sm'>{filteredImages[selectedIndex].size}, {filteredImages[selectedIndex].medium}, {filteredImages[selectedIndex].year}</p>
-          <p className='text-sm'>{filteredImages[selectedIndex].price}</p>
-        </div>
-
-        {/* Right Arrow (mobile version next to text, large screen on sides) */}
-        <button 
-          onClick={showNextImage} 
-          className='text-white text-3xl hover:text-gray-300 bg-transparent border-none ml-4 lg:ml-0 lg:absolute lg:right-4 lg:top-1/2 lg:transform lg:-translate-y-1/2 lg:translate-x-[calc(100%+1rem)]' // Stick to the right border
-        >
-          <BsChevronRight />
-        </button>
-      </div>
-    </div>
-  </div>
-)}
+      {/* Use the Modal component */}
+      {selectedIndex !== null && (
+        <Modal
+          image={filteredImages[selectedIndex]}
+          isOpen={selectedIndex !== null}
+          onClose={closeModal}
+          onNext={showNextImage}
+          onPrev={showPrevImage}
+        />
+      )}
     </div>
   );
 };
